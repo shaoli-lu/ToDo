0e0d4cd4af62e8dc612d7c8c332440371765581a
Shaoli Lu <shaoli_lu@yahoo.co>
2020-09-22 19:06:09 -0500

    initial commit

---
+++ toDo.js
@@ -0,0 +1,22 @@
+let myForm = document.getElementById("myForm")
+let myField = document.getElementById("myField")
+let myList = document.getElementById("myList")
+myForm.addEventListener("submit", 
+(e) => 
+{
+    e.preventDefault()
+    createItem(myField.value)
+
+})
+
+function createItem(x) {
+    let myHTML = `<li>${x} <button onclick="deleteItem(this)">Delete</button></li>`
+myList.insertAdjacentHTML("beforeend", myHTML)
+myField.value=""
+myField.focus()
+}
+
+function deleteItem(x) {
+    x.parentElement.remove()
+
+}
