f5e0466d3da374e47b56bda4f8e8e8d56858eba2
Shaoli Lu <shaoli_lu@yahoo.co>
2020-10-22 20:36:58 -0500

    Persist locally

--- toDo.js
+++ toDo.js
@@ -1,27 +1,64 @@
 let myForm = document.getElementById("myForm")
 let myField = document.getElementById("myField")
 let myList = document.getElementById("myList")
+let itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
+localStorage.setItem("items", JSON.stringify(itemsArray));
+
+let data = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
+
+function load() {
+    clearList()
+
+    data.forEach(item => {
+        createItem(item)
+      });
+}
+
+
 function setFocus() {
     myField.focus()
 }
+function clearList() {
+    var e = document.querySelector("ul"); 
+        e.innerHTML = ""; 
+}
 setFocus()
+load();
 
 myForm.addEventListener("submit", 
 (e) => 
 {
     e.preventDefault()
-    createItem(myField.value)
-
+    if (myField.value !=="")
+    {
+    data.push(myField.value);
+    localStorage.setItem("items", JSON.stringify(data));
+    load();
+    }
+      myField.value=""
+      myField.focus()
+      myHTML = "";
+   
 })
 
 function createItem(x) {
-    let myHTML = `<li><button class = "btn" style="color: black; background-color: red; " onclick="deleteItem(this)">Delete</button> ${x}</li>`
-    if (myField.value !="") myList.insertAdjacentHTML("beforeend", myHTML)
-    myField.value=""
-    myField.focus()
+    
+    let myHTML = `<li><button class = "btn" style="color: black; background-color: red; " onclick="deleteItem('${x}')">Delete</button> ${x}</li>`
+    myList.insertAdjacentHTML("beforeend", myHTML)
+
 }
 
-function deleteItem(x) {
-    x.parentElement.remove()
 
+
+function deleteItem(x) {
+    clearList();
+    data = data.filter(function(newList)
+    {
+        return newList !== x;
+    }
+    );
+    
+    localStorage.setItem("items", JSON.stringify(data));
+    
+    load();
 }
